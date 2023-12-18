520a654bb751fb05a67b69ed5252e75ded8012a6
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-18 22:46:31 +0400

    Update overweigh.java

--- overweigh.java
+++ overweigh.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
- public class overweigh {
-  public static void main (String[] args) {
-Scanner s=new Scanner (System.in);
-System.out.println("Height:");
-int a=s.nextInt();
-System.out.println("Weight :");
-int b=s.nextInt();
+public class overweigh {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Height:");
+    int a = s.nextInt();
+    System.out.println("Weight :");
+    int b = s.nextInt();
 
-int c=a-b;
+    int c = a - b;
 
-if (c==100){
-  System.out.println("Normal weight");
+    if (c == 100) {
+      System.out.println("Normal weight");
 
+    }
+    if (c > 100) {
+      System.out.println("Underweight");
+      int d = c - 100;
+      System.out.println(d);
+
+    }
+    if (c < 100) {
+      System.out.println("Overweight");
+      int d = 100 - c;
+      System.out.println(d);
+
+    }
+  }
 }
-if (c>100){
-  System.out.println("Underweight");
-  int d=c-100;
-  System.out.println(d);
-
-}
-if (c<100){
-  System.out.println("Overweight");
-  int d=100-c;
-  System.out.println(d);
 
 }
  }
