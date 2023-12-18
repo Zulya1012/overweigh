eab90abf1cdf3eb6badb7d51df6745a80135c0a7
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-18 22:21:10 +0400

    Update overweigh.java

--- overweigh.java
+++ overweigh.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
  public class overweigh {
   public static void main (String[] args) {
 Scanner s=new Scanner (System.in);
-System.out.println("Rost :");
+System.out.println("Height:");
 int a=s.nextInt();
-System.out.println("Ves :");
+System.out.println("Weight :");
 int b=s.nextInt();
 
 int c=a-b;
 
 if (c==100){
-  System.out.println("ves normalniy");
+  System.out.println("Normal weight");
 
 }
 if (c>100){
-  System.out.println("ne do ves");
+  System.out.println("Underweight");
   int d=c-100;
   System.out.println(d);
 
 }
 if (c<100){
-  System.out.println("lishniy ves");
+  System.out.println("Overweight");
   int d=100-c;
   System.out.println(d);
 
